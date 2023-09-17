9aa4d4ef191402b6a923dd509a0851a1a185e022
Kian Bagherlee <55768636+Ninsta22@users.noreply.github.com>
2023-09-17 16:28:13 +0000

    Attempted to Fix Linter

--- src/panda_stat.py
+++ src/panda_stat.py
@@ -1,11 +1,13 @@
 import sys
 import os
+try:
+    from library.lib import run_statistics
+except ModuleNotFoundError:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    sys.path.insert(0, parent_dir)
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
-sys.path.insert(0, parent_dir)
-
-from library.lib import run_statistics
+    from library.lib import run_statistics
 
 
 def getDescStats():
